9c0a5292a7ab0c87df1dfa9f9fa377e3e80c79ef
kilonzijnr <vkilonzi05@gmail.com>
2021-11-02 08:26:33 +0300

    changed some form fields

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -7,15 +7,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 #Create your forms here
 
-class ReqistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
     """A Form for user registration"""
     class Meta:
         model = User
-        fields = ['first_name', 'sur_name', 'email','username', 'password', 'confirm_password']
+        fields = ['first_name', 'last_name', 'email','username', 'password', 'password2']
 
         widgets = {
             'first_name':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"First Name", 'label': 'First Name'}),
-            'sur_name':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Second Name", 'label': 'Sur Name'}),
+            'last_name':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Second Name", 'label': 'Sur Name'}),
             'email':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Email Address", 'label': 'Email Address'}),
             'username':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Username", 'label': 'Username'}),
             'password':forms.PasswordInput(attrs = {'class':'form-control names','type':'password', 'placeholder':"Password", 'label': 'Password'}),
